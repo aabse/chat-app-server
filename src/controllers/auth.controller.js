4b2744e5ea33cbf37dfc9604e54440c7b8297469
Angel Abse Hidalgo <angel.abse@tinet.cl>
2023-10-30 00:21:42 -0300

    Added password encriptation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,25 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
   const {email, password} = req.body
 
   try {
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const newUser = new User({
       email,
-      password
+      password: passwordHash
     })
 
     const userSaved = await newUser.save()
-    res.json(userSaved)
+    res.json({
+      id: userSaved.id,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt
+    })
   } catch (error) {
     console.log(error)
   }
