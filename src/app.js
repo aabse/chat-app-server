d947e8550d03bcc0874865a04b4c88e5b40509de
Angel Abse Hidalgo <angel.abse@tinet.cl>
2023-10-29 22:39:46 -0300

    Added auth routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import logger from 'morgan'
+import authRoutes from './routes/auth.routes.js'
 
 const app = express()
+
+// logger
 app.use(logger('dev'))
+
+// routes
+app.use(authRoutes)
 
 export default app
