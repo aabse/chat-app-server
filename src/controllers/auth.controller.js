ae6a8dfd816217e75f30938d5777867d7196c06c
Angel Abse Hidalgo <angel.abse@tinet.cl>
2023-10-31 00:09:12 -0300

    Added login and logout features

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -37,6 +37,40 @@ export const register = async (req, res) => {
   }
 }
 
-export const login = (req, res) => {
-  res.send('login')
+export const login = async (req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const userFound = await User.findOne({email})
+
+    if (!userFound) return res.status(400).json({
+      message: 'Invalid credentials'
+    })
+
+    const isMatch = await bcrypt.compare(password, userFound.password)
+    if (!isMatch) res.status(400).json({
+      message: 'Invalid credentials'
+    })
+
+    const token = await createAccessToken({
+      id: userFound._id
+    })
+
+    res.cookie('token', token)
+    res.json({
+      id: userFound._id,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const logout = (req, res) => {
+  res.cookie('token', '', {
+    expires: new Date(0)
+  })
+  return res.sendStatus(200)
 }
